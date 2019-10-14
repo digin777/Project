7fbff7b16bf4d113c2d32f27765ecb20aec6914c
Digin Antony <Diginantony777@gmail.com>
2019-10-14 23:06:16 +0530

    telegram service script added and boot is modified

--- src/boot.py
+++ src/boot.py
@@ -4,11 +4,14 @@ def startElsaRecoganize():
     os.system("python3 /home/pi/git/project/Project/src/libs/speech/ElsaSpeechRecognizer.py")
 def startElsaSpeek():
     os.system("python3 /home/pi/git/project/Project/src/libs/speech/ElsaSpeechSynthesizer.py")
+def startTelegram():
+    os.system("python3 /home/pi/git/project/Project/src/telegrambot.py")
 def startBrain():
     os.system("python3 /home/pi/git/project/Project/src/Brain.py")
 p1= Process(target=startBrain,args=())
 p2= Process(target=startElsaSpeek,args=())
-p3= Process(target=startElsaRecoganize,args=())
+p3= Process(target=startTelegram,args=())
+p4= Process(target=startElsaRecoganize,args=())
 
 print("BOOTING",end='')
 time.sleep(2)
@@ -24,8 +27,12 @@ p2.start()
 print('Starting Speech Sythesis Engine...')
 time.sleep(2)
 p3.start()
+print('Starting Telegramservices...')
+time.sleep(1)
+p4.start()
 print('Starting Speech Recoganizing Engine...')
 p1.join()
 p2.join()
 p3.join()
+p4.join()
     
---
+++ src/telegrambot.py
@@ -0,0 +1,83 @@
+#coder :- DIJIN ANTONY
+
+import sys
+import time,socket,threading
+import random
+import datetime
+import telepot
+sys.path.append("/home/pi/git/project/Project/src/")
+from libs.IoT import IOT
+global s_msg,status
+s_msg=0
+status={'lbed':'OFF','fbed':'OFF','lmain':'OFF','fmain':'OFF'}
+HOST = '127.0.0.1'
+PORT=6373
+def on(pin):
+       IOT.performIOT(pin,'ON')
+       return 'ON'
+def off(pin):
+        IOT.performIOT(pin,'OFF')
+        return 'OFF'
+def passmessage(message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((HOST, 6687))
+                s.sendall(("an Message is Recived \n"+message).encode())
+            except (socket.gaierror, ConnectionRefusedError) as e:
+                print("Unable Open the Socket")
+            else:
+                print("The Message is successfully sent")
+
+def handle(msg):
+    global s_msg,status
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    port_map={'lbed':10,'fbed':12,'lmain':16,'fmain':18}
+    
+    print ('Got command: %s' % command,chat_id)
+    if 'lbedon' in command.lower() :
+       bot.sendMessage(chat_id, on(10))
+       status['lbed']='ON'
+    elif 'lbedoff' in command.lower():
+        bot.sendMessage(chat_id, off(10))
+        status['lbed']='OFF'
+    elif 'fbedon' in command.lower():
+        bot.sendMessage(chat_id, on(12))
+        status['fbed']='ON'
+    elif 'fbedoff' in command.lower():
+        bot.sendMessage(chat_id, off(12))
+        status['fbed']='OFF'
+    elif 'lmainon' in command.lower():
+       bot.sendMessage(chat_id, on(16))
+       status['lmain']='ON'
+    elif 'lmainoff' in command.lower():
+       bot.sendMessage(chat_id, off(16))
+       status['lmain']='OFF'
+    elif 'fmainon' in command.lower():
+        bot.sendMessage(chat_id, on(18))
+        status['fmain']='ON'
+    elif 'fmainoff' in command.lower():
+        bot.sendMessage(chat_id, off(18))
+        status['fmain']='OFF'
+    elif command.lower() == '/start':
+        bot.sendMessage(chat_id,"Welcome to ELSA")
+    elif command.lower() == '/message':
+        bot.sendMessage(chat_id,"Send Message")
+        s_msg=int(msg['message_id'])
+    elif int(msg['message_id'])==s_msg+2:
+        message=command.lower()
+        bot.sendMessage(chat_id,"Message is Recived")
+        threading.Thread(target=passmessage,args=(message,)).start()
+    elif command.lower()=='/status':
+        ret_str=''
+        for i,v in status.items():
+            ret_str+=i+':'+v+'\n'
+        bot.sendMessage(chat_id,ret_str)
+        
+
+bot = telepot.Bot('913652637:AAEq4thKkkkFmqJZpIcDzQbtbls2gNFupuo')
+bot.message_loop(handle)
+print ('I am listening...')
+
+while 1:
+     time.sleep(10)
